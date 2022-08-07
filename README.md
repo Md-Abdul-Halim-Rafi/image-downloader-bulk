7573fb0d10b83f321f296acbc4bd3ffcf9bcb8f2
Md-Abdul-Halim-Rafi <mdabdulhalimrafi@gmail.com>
2022-08-07 23:13:50 +0600

    asdf

---
+++ README.md
@@ -0,0 +1,14 @@
+### Sample Dataset in CSV format
+
+```
+name,image_url
+test name1,https://testurl1.png
+test name2,https://testurl2.png
+```
+
+### Run the project
+
+- `npm i`
+- rename the dataset of csv format as **"data.csv"**
+- if there is no **"images"** directory, create one
+- `npm start`
--- server.js
+++ server.js
@@ -1,22 +1,56 @@
 const fs = require("fs");
 const { default: axios } = require("axios");
 
+const safeMimeTypes = ["jpg", "jpeg", "png", "gif", "webp", "svg"];
+
 const startDownload = async () => {
 
     try {
 
-        const { data } = await axios.get(
-            "https://turtles-cdn.s3-ap-southeast-1.amazonaws.com/Foodie-Goodie-5039/timeline/avatar-c5b080aa.jpeg",
-            { responseType: "arraybuffer" }
-        );
+        let images = fs.readFileSync("./data.csv", "utf8");
         
-        fs.writeFileSync("./test.jpeg", data);
+        images = images.split(/\r?\n/);
 
+        for (let i = 1; i < images.length; i++) {
+            
+            const image = images[i].split(",");
+            const name = image[0].split(" ").join("-").toLocaleLowerCase();
+            const url = image[1];
+
+            if (i < 275) continue;
+
+            if (name && url) {
+
+                let mimetype = url.split(".").pop();
+
+                if (!safeMimeTypes.includes(mimetype)) {
+                    mimetype = "jpeg";
+                }
+
+                try {
+
+                    const { data } = await axios.get(
+                        url,
+                        { responseType: "arraybuffer" }
+                    );
+    
+                    const filename = `./images/${name}.${mimetype}`;
+    
+                    fs.writeFileSync(filename, data);
+    
+                    console.log(`${i} => ${filename}`);
+
+                } catch (err) {
+                    console.error(`Error for: ${i}`);
+                    continue;
+                }
+            }
+        }
+        
         console.log("DONE!!!");
 
     } catch (err) {
-        
-        console.error(err.response);
+        console.error(err);
     }
 }
 
